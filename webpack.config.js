114657bec120e9f8f141401474a810926f2f67dc
Persis Ratouis <persis.ratouis@gmail.com>
2018-10-03 10:52:57 -0500

    antd webpack basic config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 var webpack = require('webpack');
 const path = require('path');
+
+const fs = require('fs');
+/*
+* tutorial and tips by Geoff Miller
+https://medium.com/@GeoffMiller/how-to-customize-ant-design-with-react-webpack-the-missing-guide-c6430f2db10f
+*/
+const lessToJs = require('less-vars-to-js');
+const lessPath = path.join(__dirname,'./reactApp/styles/ant-default-vars.less');
+const themeVars = lessToJs(fs.readFileSync(lessPath, 'utf8'));
+
 module.exports = {
+  mode: 'development',
   entry: './reactApp/app.js',
   output: {
     path: path.join(__dirname,'./build'),
@@ -9,7 +20,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\m?js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
@@ -17,9 +28,25 @@ module.exports = {
             presets: [
               'env',
               'react'
+            ],
+            plugins: [
+              ['import', {libraryName: "antd", style: true}]
             ]
           }
         }
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {loader: "style-loader"},
+          {loader: "css-loader"},
+          {loader: "less-loader",
+            options: {
+              modifyVars: themeVars,
+              javascriptEnabled: true
+            }
+          }
+        ]
       }
     ]
   },
